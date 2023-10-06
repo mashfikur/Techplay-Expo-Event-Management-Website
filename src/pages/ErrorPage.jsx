80bd16f06ddf1b4a235acdcad48088b265fc457c
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-06 20:05:14 +0600

    error page added

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,7 +1,17 @@
 const ErrorPage = () => {
   return (
     <div>
-      <h3>this is error page</h3>
+      <div className=" p-4  flex flex-col text-center items-center justify-center min-h-screen xl:min-h-[80vh] ">
+        <h3 className="uppercase font-medium text-xl">oops ! page not found</h3>
+        <h1 className="font-bold text-[10rem] -my-10 flex -space-x-6 ">
+          <span className="">4</span>
+          <span className="text-yellow-500">0</span>
+          <span className="">4</span>
+        </h1>
+        <p className="uppercase font-medium text-xl">
+          we are sorry but the page you requested was not found
+        </p>
+      </div>
     </div>
   );
 };
