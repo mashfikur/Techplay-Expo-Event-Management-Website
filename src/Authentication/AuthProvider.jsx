b56d5c187aa27e98a235d2db7cd268803fc8e0f3
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-10-07 13:28:15 +0600

    authenctication setup completed

---
+++ src/Authentication/AuthProvider.jsx
@@ -0,0 +1,56 @@
+import PropTypes from "prop-types";
+import { createContext, useEffect, useState } from "react";
+import auth from "../firebase/firebase.config";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const userSignIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const userSignOut = () => {
+    return signOut(auth);
+  };
+
+  //observing current user
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
+    });
+
+    return () => unSubscribe();
+    
+  }, []);
+
+  const authInfo = { user, loading, createUser, userSignIn, userSignOut };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export default AuthProvider;
